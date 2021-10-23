32e983d7ad9d097ff039bb1bee3d88830b2aa10a
PoonYingWai <poonyingwai4@gmail.com>
2021-10-24 01:17:19 +0900

    fix security

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!oso86k5%-s-6ea54-zks5)ez4et&l6s+=j^36nam0n$z*hekq'
+with open('secret.txt', 'r') as f:
+    secret_key = f.read()
+
+print(secret_key)
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
